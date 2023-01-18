7bcb0f99f2b8e05dde8fd02962ea92c1a853aff3
Mauricio_Garzon <93349015+MauricioGarzon@users.noreply.github.com>
2023-01-18 14:38:19 -0500

    second commit

--- src/shapetime/Shapes.java
+++ src/shapetime/Shapes.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package final_exam;
-
+package shapetime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- src/shapetime/Shapetime.java
+++ src/shapetime/Shapetime.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package final_exam;
+package shapetime;
 
 /**
  *
  * @author Mauricio Garzon Llanos
  * ID: 1172366
  */
-public class Final_Exam 
+public class Shapetime
 {
 
     /**
